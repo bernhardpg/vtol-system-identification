4b17b2fead2924130fac8001ee9ca2f17b459965
Bernhard Paus Græsdal <bernhardpg@gmail.com>
2021-05-31 19:18:47 +0200

    Write full state model

--- models/full_state_model_c.c
+++ models/full_state_model_c.c
@@ -31,7 +31,7 @@
 #include "math.h"
 
 /* Specify the number of outputs here. */
-#define NY 5
+#define NY 7
 // Taken from this page: https://stackoverflow.com/questions/3437404/min-and-max-in-c
 #define max(a,b) \
    ({ __typeof__ (a) _a = (a); \
@@ -104,6 +104,10 @@ void compute_dx(
 		elevator_trim = p[12];
 		rudder_trim = p[13];
 
+    // ********
+    // Parameters
+    // ********
+
     double *c_L_0, *c_L_alpha, *c_L_q, *c_L_delta_e; // Lift parameters
     c_L_0 = p[14];
     c_L_alpha = p[15];
@@ -123,70 +127,117 @@ void compute_dx(
     c_m_q = p[25];
     c_m_delta_e = p[26];
 
+		// Y-aerodynamic force
+    double *c_Y_beta, *c_Y_p, *c_Y_r, *c_Y_delta_a, *c_Y_delta_r;
+    c_Y_beta = p[27];
+    c_Y_p = p[28];
+    c_Y_r = p[29];
+    c_Y_delta_a = p[30];
+    c_Y_delta_r = p[31];
+
+		// Moment around x-axis
+    double *c_l_beta, *c_l_p, *c_l_r, *c_l_delta_a, *c_l_delta_r;
+    c_l_beta = p[32];
+    c_l_p = p[33];
+    c_l_r = p[34];
+    c_l_delta_a = p[35];
+    c_l_delta_r = p[36];
+
+		// Moment around z-axis
+    double *c_n_beta, *c_n_p, *c_n_r, *c_n_delta_a, *c_n_delta_r;
+    c_n_beta = p[37];
+    c_n_p = p[38];
+    c_n_r = p[39];
+    c_n_delta_a = p[40];
+    c_n_delta_r = p[41];
+
     // *******
     // State and input
     // *******
     // State: [q_attitude, ang_v_body, vel_body]
 
-    // q_attitude = q0, q1, q2, q3
-    double q0, q2;
-    q0 = x[0];
-    q2 = x[1];
+    // q_attitude = e0, e1, e2, e3
+    double e0, e1, e2, e3;
+    e0 = x[0];
+    e1 = x[1];
+    e2 = x[2];
+    e3 = x[3];
 
-		double quat_norm = sqrt(pow(q0,2) + pow(q2,2)); // NOTE: q1 and q3 are both zero (always) for long model
-		q0 = q0 / quat_norm;
-		q2 = q2 / quat_norm;
+		// Make sure quaternion remains a unit quaternion
+		double quat_norm = sqrt(pow(e0,2) + pow(e1,2) + pow(e2,2) + pow(e3,2));
+		e0 = e0 / quat_norm;
+		e1 = e1 / quat_norm;
+		e2 = e2 / quat_norm;
+		e3 = e3 / quat_norm;
 
-    double ang_q;
-    ang_q = x[2];
+    double ang_p, ang_q, ang_r;
+    ang_p = x[4];
+    ang_q = x[5];
+    ang_r = x[6];
 
     // v_body = u, v, w
-    double vel_u, vel_w;
-    vel_u = x[3];
-    vel_w = x[4];
+    double vel_u, vel_v, vel_w;
+    vel_u = x[7];
+    vel_v = x[8];
+    vel_w = x[9];
 
 		// Elevator dynamics
-		double delta_e;
-		delta_e = x[5];
+		double delta_a, delta_e, delta_r;
+		delta_a = x[10];
+		delta_e = x[11];
+		delta_r = x[12];
 
-    double delta_e_sp, n_p;
-    delta_e_sp = u[0] - elevator_trim[0];
-    n_p = u[1];
+    double delta_a_sp, delta_e_sp, delta_r_sp;
+    delta_a_sp = u[0] - aileron_trim[0];
+    delta_e_sp = u[1] - elevator_trim[0];
+    delta_r_sp = u[2] - rudder_trim[0];
 
     // ******
     // Forces
     // ******
 
-    // Calculate AoA, assuming no wind
-    double V = sqrt(pow(vel_u,2) + pow(vel_w,2));
+    double V = sqrt(pow(vel_u,2) + pow(vel_v,2) + pow(vel_w,2));
     double alpha = atan2(vel_w, vel_u);
+		double beta = asin(vel_v / V);
 
     // Gravitational force
 		double m_times_g = m[0] * g[0];
-    double F_g_x, F_g_z;
-    F_g_x = - 2 *  m_times_g * q0 * q2;
-    F_g_z = m_times_g * (pow(q0, 2) - pow(q2, 2));
+    double F_g_x, F_g_y, F_g_z;
+
+    F_g_x = 2 * m_times_g * (e1 * e3 - e0 * e2);
+    F_g_y = 2 * m_times_g * (e2 * e3 + e0 * e1);
+    F_g_z = m_times_g * (pow(e0, 2) - pow(e1, 2) - pow(e2, 2) + pow(e3, 2));
 
     // Aerodynamic forces
     double c_L = c_L_0[0] + c_L_alpha[0] * alpha
       + c_L_q[0] * (nondim_constant_lon[0]) * ang_q
       + c_L_delta_e[0] * delta_e;
-    double F_lift = half_rho_planform[0] * pow(V, 2) * c_L;
+
+    double c_Y = beta * c_Y_beta[0]
+			+ c_Y_p[0] * nondim_constant_lat[0] * ang_p
+			+ c_Y_r[0] * nondim_constant_lat[0] * ang_r
+			+ c_Y_delta_a[0] * delta_a
+			+ c_Y_delta_r[0] * delta_r;
 
     double c_D = c_D_p[0] + c_D_alpha[0] * alpha + c_D_alpha_sq[0] * pow(alpha, 2)
       + c_D_q[0] * (nondim_constant_lon[0]) * ang_q
       + c_D_delta_e[0] * delta_e;
+
+    double F_lift = half_rho_planform[0] * pow(V, 2) * c_L;
     double F_drag = half_rho_planform[0] * pow(V, 2) * c_D;
 
-    double X, Z;
+    double X, Y, Z;
 
     // Rotate from stability frame to body frame
     X = -cos(alpha) * F_drag + sin(alpha) * F_lift;
+    Y = half_rho_planform[0] * pow(V, 2) * wingspan[0] * c_Y;
+    // Rotate from stability frame to body frame
     Z = -sin(alpha) * F_drag - cos(alpha) * F_lift;
 
     // Sum all forces
-    double F_tot_x, F_tot_z;
+    double F_tot_x, F_tot_y, F_tot_z;
     F_tot_x = F_g_x + X;
+    F_tot_y = F_g_y + Y;
     F_tot_z = F_g_z + Z;
 
     // ******
@@ -194,48 +245,89 @@ void compute_dx(
     // ******
 
     // Aerodynamic moments
+    double c_l = beta * c_l_beta[0]
+			+ c_l_p[0] * nondim_constant_lat[0] * ang_p
+			+ c_l_r[0] * nondim_constant_lat[0] * ang_r
+			+ c_l_delta_a[0] * delta_a
+			+ c_l_delta_r[0] * delta_r;
+
     double c_m = c_m_0[0]
       + c_m_alpha[0] * alpha
       + c_m_q[0] * nondim_constant_lon[0] * ang_q
       + c_m_delta_e[0] * delta_e;
 
-    double M;
+    double c_n = beta * c_n_beta[0]
+			+ c_n_p[0] * nondim_constant_lat[0] * ang_p
+			+ c_n_r[0] * nondim_constant_lat[0] * ang_r
+			+ c_n_delta_a[0] * delta_a
+			+ c_n_delta_r[0] * delta_r;
+
+    double L, M, N;
+    L = half_rho_planform[0] * pow(V, 2) * wingspan[0] * c_l;
     M = half_rho_planform[0] * pow(V, 2) * chord[0] * c_m;
+    N = half_rho_planform[0] * pow(V, 2) * wingspan[0] * c_n;
 
     // *******
     // Kinematics
     // *******
 
-    double q0_dot, q1_dot, q2_dot, q3_dot;
-    q0_dot = - 0.5 * q2 * ang_q;
-    q2_dot = 0.5 * q0 * ang_q;
+    double e0_dot, e1_dot, e2_dot, e3_dot;
+    e0_dot = 0.5 * (- e1 * ang_p - e2 * ang_q - e3 * ang_r);
+    e1_dot = 0.5 * (e0 * ang_p - e3 * ang_q + e2 * ang_r);
+    e2_dot = 0.5 * (e3 * ang_p + e0 * ang_q - e1 * ang_r);
+    e3_dot = 0.5 * (- e2 * ang_p + e1 * ang_q + e0 * ang_r);
 
-    dx[0] = q0_dot;
-    dx[1] = q2_dot;
+    dx[0] = e0_dot;
+    dx[1] = e1_dot;
+    dx[2] = e2_dot;
+    dx[3] = e3_dot;
 
     // *******
     // Dynamics
     // *******
 
-    double ang_q_dot;
-    ang_q_dot = (1 / J_yy[0]) * M;
+    double ang_p_dot, ang_q_dot, ang_r_dot;
+    ang_p_dot = lam_1 * ang_p * ang_q - lam_2 * ang_q * ang_r
+			+ lam_3 * L + lam_4 * N;
+    ang_q_dot = lam_5 * ang_p * ang_r - lam_6 * (pow(ang_p, 2) - pow(ang_r, 2))
+			+ (1 / J_yy[0]) * M;
+    ang_r_dot = lam_7 * ang_p * ang_q - lam_1 * ang_q * ang_r
+			+ lam_4 * L + lam_8 * N;
 
-    dx[2] = ang_q_dot;
+    dx[4] = ang_p_dot;
+    dx[5] = ang_q_dot;
+    dx[6] = ang_r_dot;
 
-    double vel_u_dot, vel_w_dot;
-    vel_u_dot = (1/m[0]) * F_tot_x - ang_q * vel_w;
-    vel_w_dot = (1/m[0]) * F_tot_z + ang_q * vel_u;
+    double vel_u_dot, vel_v_dot, vel_w_dot;
+    vel_u_dot = ang_r * vel_v - ang_q * vel_w + (1/m[0]) * F_tot_x;
+    vel_v_dot = ang_p * vel_w - ang_r * vel_u + (1/m[0]) * F_tot_y;
+    vel_w_dot = ang_q * vel_u - ang_p * vel_v + (1/m[0]) * F_tot_z;
 
-    dx[3] = vel_u_dot;
-    dx[4] = vel_w_dot;
+    dx[7] = vel_u_dot;
+    dx[8] = vel_v_dot;
+    dx[9] = vel_w_dot;
 
-		// NOTE: Elevator is not an output, as it is not measured
-		double delta_e_dot;
+		// Model control surfaces as rate limited first order responses
+		double delta_a_dot, delta_e_dot, delta_r_dot;
+
+		delta_a_dot = bound(
+				-1 / servo_time_const[0] * delta_a + 1 / servo_time_const[0] * delta_a_sp,
+				-servo_rate_lim[0],
+				servo_rate_lim[0]);
+
 		delta_e_dot = bound(
 				-1 / servo_time_const[0] * delta_e + 1 / servo_time_const[0] * delta_e_sp,
 				-servo_rate_lim[0],
 				servo_rate_lim[0]);
-    dx[5] = delta_e_dot;
+
+		delta_r_dot = bound(
+				-1 / servo_time_const[0] * delta_r + 1 / servo_time_const[0] * delta_r_sp,
+				-servo_rate_lim[0],
+				servo_rate_lim[0]);
+
+    dx[10] = delta_a_dot;
+    dx[11] = delta_e_dot;
+    dx[12] = delta_r_dot;
 }
 
 /* Output equations. */
@@ -266,6 +358,9 @@ void compute_y(
     y[2] = x[2];
     y[3] = x[3];
     y[4] = x[4];
+    y[5] = x[5];
+    y[6] = x[6];
+		// Control surface deflections are not outputs, as these are not measured
 }
 
 
