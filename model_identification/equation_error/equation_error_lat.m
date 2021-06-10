5c62f5c4163c57ae1e02c4c9e20fc6535e07320a
Bernhard Paus Græsdal <bernhardpg@gmail.com>
2021-06-10 22:11:24 +0200

    Write full model

--- model_identification/equation_error/equation_error_lat.m
+++ model_identification/equation_error/equation_error_lat.m
@@ -1,240 +1,42 @@
 clc; clear all; close all;
-maneuver_types = ["roll_211" "yaw_211"];
+maneuver_types = ["roll_211", "yaw_211"];
+data_type = "train";
 load_data;
 
-%%
-%%%
-% Find most relevant terms for c_Y:
-%%%
-
-clc;
-N = length(c_Y);
-
-X = [ones(N, 1)]; % Regressor
-z = c_Y; % Output measurements
-indep_vars_str = "1";
-[~, ~, ~, ~, R_sq_prev] = stepwise_regression_round(X, z, indep_vars_str, 0);
-
-vars_to_test_str = "v_hat p_hat r_hat delta_a delta_r";
-vars_to_test = [v_hat p_hat r_hat delta_a delta_r];
-explore_next_var(z, vars_to_test, vars_to_test_str);
-
-X = [ones(N, 1) v_hat]; % Regressor
-z = c_Y; % Output measurements
-indep_vars_str = "1 v_hat";
-[~, ~, ~, ~, R_sq_prev] = stepwise_regression_round(X, z, indep_vars_str, R_sq_prev);
-
-vars_to_test_str = "p_hat r_hat delta_a delta_r";
-vars_to_test = [p_hat r_hat delta_a delta_r];
-explore_next_var(z, vars_to_test, vars_to_test_str);
-
-X = [ones(N, 1) v_hat delta_r]; % Regressor
-z = c_Y; % Output measurements
-indep_vars_str = "1 v_hat delta_r";
-[~, ~, ~, ~, R_sq_prev] = stepwise_regression_round(X, z, indep_vars_str, R_sq_prev);
-
-vars_to_test_str = "p_hat r_hat delta_a";
-vars_to_test = [p_hat r_hat delta_a];
-explore_next_var(z, vars_to_test, vars_to_test_str);
-
-% delta_a gives too low increase in R_sq
-X = [ones(N, 1) v_hat delta_a delta_r]; % Regressor
-z = c_Y; % Output measurements
-indep_vars_str = "1 v_hat delta_a delta_r";
-[~, ~, ~, ~, R_sq_prev] = stepwise_regression_round(X, z, indep_vars_str, R_sq_prev);
-
-vars_to_test_str = "p_hat r_hat delta_a";
-vars_to_test = [p_hat r_hat delta_a];
-explore_next_var(z, vars_to_test, vars_to_test_str);
-
-% r_hat gives too low increase
-X = [ones(N, 1) v_hat r_hat delta_r]; % Regressor
-z = c_Y; % Output measurements
-indep_vars_str = "1 v_hat r_hat delta_r";
-[~, ~, ~, ~, R_sq_prev] = stepwise_regression_round(X, z, indep_vars_str, R_sq_prev);
-
-% p_hat gives somewhat good increase
-X = [ones(N, 1) v_hat p_hat delta_r]; % Regressor
-z = c_Y; % Output measurements
-indep_vars_str = "1 v_hat p_hat delta_r";
-[~, ~, ~, ~, R_sq_prev] = stepwise_regression_round(X, z, indep_vars_str, R_sq_prev);
-
-disp("Trying nonlinear terms")
-vars_to_test_str = "v_hat.^2 p_hat.^2 delta_r.^2";
-vars_to_test = [v_hat.^2 p_hat.^2 delta_r.^2];
-explore_next_var(z, vars_to_test, vars_to_test_str);
-
-% Final model design
-X = [ones(N, 1) v_hat p_hat delta_r]; % Regressor
-z = c_Y; % Output measurements
-indep_vars_str = "1 v_hat p_hat delta_r";
-[c_Y_hat, th_hat, cov_th, F0, R_sq] = stepwise_regression_round(X, z, indep_vars_str, R_sq_prev);
-
-th_hat = num2cell(th_hat);
-[c_Y_0, c_Y_v, c_Y_p, c_Y_delta_r] = th_hat{:}
-
-fig = figure;
-fig.Position = [100 100 1700 500];
-plot(t_plot, c_Y, '--'); hold on;
-xlabel("time [s]")
-plot(t_plot, c_Y_hat); hold on
-legend("c_Y", "c_Y (predicted)")
+% Create explanatory variables
+[p_hat, q_hat, r_hat, u_hat, v_hat, w_hat] = calc_explanatory_vars(p, q, r, u, v, w);
+t_plot = 0:dt:length(t)*dt-dt;
 
 %%
+%%%%%%%%%%%%%%%%%%%%%%%
+% Stepwise regression %
+%%%%%%%%%%%%%%%%%%%%%%%
 
-%%%
-% Find most relevant terms for c_l:
-%%%
+regr = [p_hat r_hat v_hat delta_a delta_r]; % Basis regressors
+regr_names = ["p" "r" "v" "delta_a" "delta_r"];
+use_cross_terms = false;
 
-clc;
-N = length(c_l);
-z = c_l; % Output measurements
+clc
+z = c_Y; % output (= dependent variable)
+[th_hat, th_names, y_hat] = stepwise_regression(z, regr, regr_names, use_cross_terms);
+print_eq_error_params("c_Y", th_hat, th_names);
 
-X = [ones(N, 1)]; % Regressor
-indep_vars_str = "1";
-[~, ~, ~, ~, R_sq_prev] = stepwise_regression_round(X, z, indep_vars_str, 0);
+figure
+plot(t_plot, z, t_plot, y_hat); hold on
+legend("$z$", "$\hat{z}$", 'Interpreter','latex')
 
-vars_to_test_str = "v_hat p_hat r_hat delta_a delta_r";
-vars_to_test = [v_hat p_hat r_hat delta_a delta_r];
-explore_next_var(z, vars_to_test, vars_to_test_str);
+z = c_l; % output (= dependent variable)
+[th_hat, th_names, y_hat] = stepwise_regression(z, regr, regr_names, use_cross_terms);
+print_eq_error_params("c_l", th_hat, th_names);
 
-X = [ones(N, 1) delta_a]; % Regressor
-indep_vars_str = "1 delta_a";
-[~, ~, ~, ~, R_sq_prev] = stepwise_regression_round(X, z, indep_vars_str, R_sq_prev);
+figure
+plot(t_plot, z, t_plot, y_hat); hold on
+legend("$z$", "$\hat{z}$", 'Interpreter','latex')
 
-vars_to_test_str = "v_hat p_hat r_hat delta_r";
-vars_to_test = [v_hat p_hat r_hat delta_r];
-explore_next_var(z, vars_to_test, vars_to_test_str);
+z = c_n; % output (= dependent variable)
+[th_hat, th_names, y_hat] = stepwise_regression(z, regr, regr_names, use_cross_terms);
+print_eq_error_params("c_n", th_hat, th_names);
 
-X = [ones(N, 1) r_hat delta_a]; % Regressor
-indep_vars_str = "1 r_hat delta_a";
-[~, ~, ~, ~, R_sq_prev] = stepwise_regression_round(X, z, indep_vars_str, R_sq_prev);
-
-vars_to_test_str = "v_hat p_hat delta_r";
-vars_to_test = [v_hat p_hat delta_r];
-explore_next_var(z, vars_to_test, vars_to_test_str);
-
-X = [ones(N, 1) p_hat r_hat delta_a]; % Regressor
-indep_vars_str = "1 p_hat r_hat delta_a";
-[c_l_hat, ~, ~, ~, R_sq_prev] = stepwise_regression_round(X, z, indep_vars_str, R_sq_prev);
-
-vars_to_test_str = "v_hat delta_r";
-vars_to_test = [v_hat delta_r];
-explore_next_var(z, vars_to_test, vars_to_test_str);
-
-% Final model design
-X = [ones(N, 1) v_hat p_hat r_hat delta_a]; % Regressor
-indep_vars_str = "1 v_hat p_hat r_hat delta_a";
-[c_l_hat, th_hat, cov_th, F0, R_sq] = stepwise_regression_round(X, z, indep_vars_str, R_sq_prev);
-
-% vars_to_test_str = "delta_r";
-% vars_to_test = [delta_r];
-% explore_next_var(z, vars_to_test, vars_to_test_str);
-% % 
-% % Too low with delta_r, do not include this
-% X = [ones(N, 1) v_hat p_hat r_hat delta_a delta_r]; % Regressor
-% indep_vars_str = "1 v_hat p_hat r_hat delta_a delta_r";
-% [c_l_hat, ~, ~, ~, R_sq_prev] = stepwise_regression_round(X, z, indep_vars_str, R_sq_prev);
-
-% disp("Trying non-linear terms")
-% vars_to_test_str = "v_hat.^2 p_hat.^ r_hat.^2 delta_a.^2";
-% vars_to_test = [v_hat.^2 p_hat.^2 r_hat.^2 delta_a.^2];
-% explore_next_var(z, vars_to_test, vars_to_test_str);
-
-% % Too low with delta_a.^2
-% X = [ones(N, 1) v_hat p_hat r_hat delta_a delta_a.^2]; % Regressor
-% indep_vars_str = "1 v_hat p_hat r_hat delta_a delta_a.^2";
-% [c_l_hat, ~, ~, ~, R_sq_prev] = stepwise_regression_round(X, z, indep_vars_str, R_sq_prev);
-% 
-% % Too low with v_hat.^2
-% X = [ones(N, 1) v_hat v_hat.^2 p_hat r_hat delta_a]; % Regressor
-% indep_vars_str = "1 v_hat v_hat.^2 p_hat r_hat delta_a";
-% [c_l_hat, ~, ~, ~, R_sq_prev] = stepwise_regression_round(X, z, indep_vars_str, R_sq_prev);
-% 
-% % Too low with p_hat.^2
-% X = [ones(N, 1) v_hat p_hat p_hat.^2 r_hat delta_a]; % Regressor
-% indep_vars_str = "1 v_hat p_hat p_hat.^2 r_hat delta_a";
-% [c_l_hat, ~, ~, ~, R_sq_prev] = stepwise_regression_round(X, z, indep_vars_str, R_sq_prev);
-% 
-% % Too low with r_hat.^2
-% X = [ones(N, 1) v_hat p_hat r_hat.^2 r_hat delta_a]; % Regressor
-% indep_vars_str = "1 v_hat p_hat r_hat.^2 r_hat delta_a";
-% [c_l_hat, ~, ~, ~, R_sq_prev] = stepwise_regression_round(X, z, indep_vars_str, R_sq_prev);
-
-
-
-th_hat = num2cell(th_hat);
-[c_l_0, c_l_v, c_l_p, c_l_r, c_l_delta_a] = th_hat{:}
-
-
-fig = figure;
-fig.Position = [100 100 1700 500];
-plot(t_plot, c_l, '--'); hold on;
-xlabel("time [s]")
-plot(t_plot, c_l_hat); hold on
-legend("c_l", "c_l (predicted)")
-
-
-%%
-
-%%%
-% Find most relevant terms for c_n:
-%%%
-
-clc;
-N = length(c_n);
-z = c_n; % Output measurements
-
-X = [ones(N, 1)]; % Regressor
-indep_vars_str = "1";
-[~, ~, ~, ~, R_sq_prev] = stepwise_regression_round(X, z, indep_vars_str, 0);
-
-vars_to_test_str = "v_hat p_hat r_hat delta_a delta_r";
-vars_to_test = [v_hat p_hat r_hat delta_a delta_r];
-explore_next_var(z, vars_to_test, vars_to_test_str);
-
-X = [ones(N, 1) v_hat]; % Regressor
-indep_vars_str = "1 v_hat";
-[~, ~, ~, ~, R_sq_prev] = stepwise_regression_round(X, z, indep_vars_str, R_sq_prev);
-
-vars_to_test_str = "p_hat r_hat delta_a delta_r";
-vars_to_test = [p_hat r_hat delta_a delta_r];
-explore_next_var(z, vars_to_test, vars_to_test_str);
-
-X = [ones(N, 1) v_hat p_hat]; % Regressor
-indep_vars_str = "1 v_hat p_hat";
-[~, ~, ~, ~, R_sq_prev] = stepwise_regression_round(X, z, indep_vars_str, R_sq_prev);
-
-vars_to_test_str = "r_hat delta_a delta_r";
-vars_to_test = [r_hat delta_a delta_r];
-explore_next_var(z, vars_to_test, vars_to_test_str);
-
-X = [ones(N, 1) v_hat p_hat delta_r]; % Regressor
-indep_vars_str = "1 v_hat p_hat delta_r";
-[~, ~, ~, ~, R_sq_prev] = stepwise_regression_round(X, z, indep_vars_str, R_sq_prev);
-
-X = [ones(N, 1) v_hat p_hat r_hat delta_r]; % Regressor
-indep_vars_str = "1 v_hat p_hat r_hat delta_r";
-[c_n_hat, th_hat, cov_th, F0, R_sq] = stepwise_regression_round(X, z, indep_vars_str, R_sq_prev);
-
-% delta_a gives too little increase in R_sq
-% X = [ones(N, 1) v_hat p_hat r_hat delta_a delta_r]; % Regressor
-% indep_vars_str = "1 v_hat p_hat r_hat delta_a delta_r";
-% [~, ~, ~, ~, R_sq_prev] = stepwise_regression_round(X, z, indep_vars_str, R_sq_prev);
-
-% little effect with nonlinear terms
-% X = [ones(N, 1) v_hat p_hat r_hat delta_r delta_r.^2]; % Regressor
-% indep_vars_str = "1 v_hat p_hat r_hat delta_r delta_r.^2";
-% [~, ~, ~, ~, R_sq_prev] = stepwise_regression_round(X, z, indep_vars_str, R_sq_prev);
-% 
-
-th_hat = num2cell(th_hat);
-[c_n_0, c_n_v, c_n_p, c_n_r, c_n_delta_r] = th_hat{:}
-
-
-fig = figure;
-fig.Position = [100 100 1700 500];
-plot(t_plot, c_n, '--'); hold on;
-xlabel("time [s]")
-plot(t_plot, c_n_hat); hold on
-legend("c_n", "c_n (predicted)")
+figure
+plot(t_plot, z, t_plot, y_hat); hold on
+legend("$z$", "$\hat{z}$", 'Interpreter','latex')
--- model_identification/model_validation/evaluate_full_model.m
+++ model_identification/model_validation/evaluate_full_model.m
@@ -2,56 +2,57 @@ clc; clear all; close all;
 
 set(groot, 'defaultAxesTickLabelInterpreter','latex'); set(groot, 'defaultLegendInterpreter','latex');
 
-maneuver_types = ["pitch_211"];
-data_type = "val";
+maneuver_types = ["roll_211" "yaw_211"];
+data_type = "train";
 load_data;
 load_const_params;
 
 % Load initial guesses
-equation_error_results_lon;
-x0 = [
-     c_X_0, c_X_w, c_X_w_sq, c_X_q, c_X_q_sq, c_X_delta_e,...
-     c_Z_0, c_Z_w, c_Z_w_sq, c_Z_delta_e,...
-     c_m_0, c_m_w, c_m_q, c_m_delta_e, c_m_delta_e_sq,...
+equation_error_results_lat;
+x_lon = [-0.1074    0.1720    2.5516  -10.0212  535.1918   -0.0493   -0.4894   -5.4394    3.3249   -0.5300    0.0115   -1.3611  -22.6064   -0.8852   -0.5934];
+
+x0_lat = [
+     c_Y_0, c_Y_p, c_Y_v, c_Y_delta_a, c_Y_delta_r,...
+     c_l_0, c_l_p, c_l_r, c_l_v, c_l_delta_a,...
+     c_n_0, c_n_p, c_n_r, c_n_v, c_n_delta_r,...
      ];
-
-% Process parameters found from output-error
-xs = readmatrix("lon_params.txt");
-xs = rmoutliers(xs);
-x = median(xs);
-param_mads = mad(xs);
-
+ 
 all_params = [const_params;
-              x'];
+              x_lon';
+              x0_lat'];
 
 % Collect recorded data
 t_seq = t;
-y_lon_seq = [theta q u w];
-y_lat_seq = [phi, psi, p, r, v];
 input_seq = [delta_a, delta_e, delta_r, n_p]; % Actuator dynamics simulated beforehand
 
 % Generate plot of all validation maneuvers
-plot_output_location = "model_identification/model_validation/validation_plots/lon_model/";
+plot_output_location = "model_identification/model_validation/validation_plots/full_model/";
 save_plot = true;
 show_plot = false;
 
 R_sq = zeros(num_maneuvers, 4);
-for maneuver_i = 8
+for maneuver_i = 3
     % Get data for desired maneuver
     [t_m, phi_m, theta_m, psi_m, p_m, q_m, r_m, u_m, v_m, w_m, a_x_m, a_y_m, a_z_m, delta_a_sp_m, delta_e_sp_m, delta_r_sp_m, delta_a_m, delta_e_m, delta_r_m, n_p_m]...
         = get_maneuver_data(maneuver_i, maneuver_start_indices, t, phi, theta, psi, p, q, r, u, v, w, a_x, a_y, a_z, delta_a_sp, delta_e_sp, delta_r_sp, delta_a, delta_e, delta_r, n_p);
-    input_seq_m = [delta_a_m delta_e_m delta_r_m n_p_m];
-    lat_state_seq_m = [phi_m, psi_m, p_m, r_m, v_m];
-    maneuver_seq_m = [t_m input_seq_m lat_state_seq_m];
+    input_seq_m = [t_m delta_a_m delta_e_m delta_r_m n_p_m];
 
     % Integrate dynamics
-    y0 = [theta_m(1) q_m(1) u_m(1) w_m(1)];
+    y0 = [phi_m(1) theta_m(1) psi_m(1) p_m(1) q_m(1) r_m(1) u_m(1) v_m(1) w_m(1)];
     tspan = [t_m(1) t_m(end)];
 
     % Integrate dynamics
-    [t_pred, y_pred] = ode45(@(t,y) lon_dynamics_c(t, y, maneuver_seq_m, all_params), tspan, y0);
+    [t_pred, y_pred] = ode45(@(t,y) full_dynamics_c(t, y, input_seq_m, all_params), tspan, y0);
     y_pred = interp1(t_pred, y_pred, tspan(1):dt:tspan(2));
 
+    R_sq_m = zeros(1,4);
+    
+    if save_plot || show_plot
+        plot_maneuver_full("val_maneuver" + maneuver_i, t_m, phi_m, theta_m, psi_m, p_m, q_m, r_m, u_m, v_m, w_m, delta_a_m, delta_e_m, delta_r_m,  delta_a_sp_m, delta_e_sp_m, delta_r_sp_m, n_p_m,...
+            t_m, y_pred,...
+            false, true, "", R_sq_m);
+    end
+    
     R_sq_theta = calc_R_sq(theta_m, y_pred(:,1));
     R_sq_q = calc_R_sq(q_m, y_pred(:,2));
     R_sq_u = calc_R_sq(u_m, y_pred(:,3));
