0629dc87c241e9ec37ef70f3550bc1ea33a8572d
Bernhard Paus Græsdal <bernhardpg@gmail.com>
2021-06-12 17:28:02 +0200

    Fix stepwise regression bug

--- model_identification/equation_error/equation_error_lon.m
+++ model_identification/equation_error/equation_error_lon.m
@@ -18,19 +18,19 @@ use_cross_terms = false;
 
 clc
 z = c_X; % output (= dependent variable)
-[th_hat, th_names, y_hat, s_sq] = stepwise_regression(z, regr, regr_names, use_cross_terms);
+[th_hat, th_names, y_hat] = stepwise_regression(z, regr, regr_names, use_cross_terms);
 print_eq_error_params("c_X", th_hat, th_names);
-disp("s_sq = " + s_sq);
 
 figure
 plot(t_plot, z, t_plot, y_hat); hold on
 legend("$z$", "$\hat{z}$", 'Interpreter','latex')
 title("c_X")
 
+%%
+
 z = c_Z; % output (= dependent variable)
-[th_hat, th_names, y_hat, s_sq] = stepwise_regression(z, regr, regr_names, use_cross_terms);
+[th_hat, th_names, y_hat] = stepwise_regression(z, regr, regr_names, use_cross_terms);
 print_eq_error_params("c_Z", th_hat, th_names);
-disp("s_sq = " + s_sq);
 
 figure
 plot(t_plot, z, t_plot, y_hat); hold on
@@ -38,9 +38,8 @@ legend("$z$", "$\hat{z}$", 'Interpreter','latex')
 title("c_Z")
 
 z = c_m; % output (= dependent variable)
-[th_hat, th_names, y_hat, s_sq] = stepwise_regression(z, regr, regr_names, use_cross_terms);
+[th_hat, th_names, y_hat] = stepwise_regression(z, regr, regr_names, use_cross_terms);
 print_eq_error_params("c_m", th_hat, th_names);
-disp("s_sq = " + s_sq);
 
 figure
 plot(t_plot, z, t_plot, y_hat); hold on
