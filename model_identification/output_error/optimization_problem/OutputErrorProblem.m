4860c6a8b148d4fccb84a1fa8dee643ca166b99c
Bernhard Paus Græsdal <bernhardpg@gmail.com>
2021-09-08 11:29:17 +0200

    Cleanup syntax

--- model_identification/output_error/optimization_problem/OutputErrorProblem.m
+++ model_identification/output_error/optimization_problem/OutputErrorProblem.m
@@ -3,119 +3,139 @@ classdef OutputErrorProblem
         DynamicsModel
         FprData
         ManeuverTypes
-        OptData = {}
+        CurrOptData = {}
         ParamPerturbation = 0.01
         NParamsLat = 18
         NOutputsLat = 4
+        CurrManeuverData
     end
     methods
         function obj = OutputErrorProblem(fpr_data, dynamics_model, maneuver_types)
             obj.FprData = fpr_data;
             obj.DynamicsModel = dynamics_model;
             obj.ManeuverTypes = maneuver_types;
-            obj.OptData.iteration_number = 0;
-            obj.OptData.reached_convergence = false;
-            obj.OptData.params.CoeffsLat = dynamics_model.CoeffsLat;
-            obj.OptData.params.CoeffsLon = dynamics_model.CoeffsLon;
+            obj.CurrOptData.iteration_number = 0;
+            obj.CurrOptData.reached_convergence = false;
+            obj.CurrOptData.params.CoeffsLat = dynamics_model.CoeffsLat;
+            obj.CurrOptData.params.CoeffsLon = dynamics_model.CoeffsLon;
         end
         
         function obj = solve(obj)
             for maneuver_type = obj.ManeuverTypes(1)
                 for maneuver_i = 1
+                    % Get maneuver data
                     maneuver = obj.FprData.training.(maneuver_type)(maneuver_i);
-                                            
-                    % Initial evaluation
-                    [obj.OptData.R_hat, obj.OptData.cost, obj.OptData.params.CoeffsLat] = evaluate_iteration_for_maneuver(obj, maneuver);
+                    obj = obj.update_curr_maneuver_data(maneuver);
                     
-                    % Run until convergence
-                    while ~obj.OptData.reached_convergence
-                        obj.OptData.iteration_number = obj.OptData.iteration_number + 1;
-                        disp("Running iteration: " + obj.OptData.iteration_number);
-                        
-                        [R_hat_new, cost_new, params_new] = evaluate_iteration_for_maneuver(obj, maneuver);
-                        obj.OptData.reached_convergence = check_for_convergence(obj, params_new, cost_new, R_hat_new);
+                    while true
+                        % Simulate current parameters
+                        obj.CurrManeuverData.y_pred = obj.sim_model(obj.CurrOptData.params.CoeffsLat, obj.CurrOptData.params.CoeffsLon);
+                        obj.CurrManeuverData.residuals = obj.calc_residuals_lat(obj.CurrManeuverData.z, obj.CurrManeuverData.y_pred);
+
+                        % Covariance matrix for Newton-Raphson step
+                        obj.CurrManeuverData.R_hat = obj.est_noise_covariance(obj.CurrManeuverData.residuals, obj.CurrManeuverData.N);
+
+                        % Calculate cost
+                        obj.CurrManeuverData.cost = obj.calc_cost_lat(obj.CurrManeuverData.R_hat, obj.CurrManeuverData.residuals);
+                        disp("Cost: " + obj.CurrManeuverData.cost);
+
+                        % Check for convergence
+                        % TODO:
+
+                        % Optimization step
+                        while true
+                            disp("Performing Newton-Raphson step")
+                            tic
+                            [obj.CurrOptData.params.CoeffsLat, information_matrix] = obj.do_newton_raphson_step();
+                            toc
+
+                            % Simulate current parameters
+                            obj.CurrManeuverData.y_pred = obj.sim_model(obj.CurrOptData.params.CoeffsLat, obj.CurrOptData.params.CoeffsLon);
+                            obj.CurrManeuverData.residuals = obj.calc_residuals_lat(obj.CurrManeuverData.z, obj.CurrManeuverData.y_pred);
+
+                            % Calculate cost
+                            obj.CurrManeuverData.cost = obj.calc_cost_lat(obj.CurrManeuverData.R_hat, obj.CurrManeuverData.residuals);
+                            disp("Cost: " + obj.CurrManeuverData.cost);
+                        end
                     end
-                    disp("Reached convergence after " + obj.OptData.iteration_number + " iterations");
+                    
+
+                    % Check for convergence
+                    
+%                     % Run until convergence
+%                     while ~obj.CurrOptData.reached_convergence
+%                         obj.CurrOptData.iteration_number = obj.CurrOptData.iteration_number + 1;
+%                         disp("Running iteration: " + obj.CurrOptData.iteration_number);
+%                         
+%                         [R_hat_new, cost_new, params_new] = evaluate_iteration_for_maneuver(obj, maneuver);
+%                         obj.CurrOptData.reached_convergence = check_for_convergence(obj, params_new, cost_new, R_hat_new);
+%                     end
+%                     disp("Reached convergence after " + obj.CurrOptData.iteration_number + " iterations");
                 end
             end
         end
         
-        function [R_hat_new, cost_new, params_new] = evaluate_iteration_for_maneuver(obj, maneuver)
-            % Get maneuver data
-            t_data_sequence = maneuver.Time;
-            tspan = [maneuver.Time(1) maneuver.Time(end)];
-            N = length(t_data_sequence);
-            z = maneuver.get_lat_state_sequence();
-            y_0 = maneuver.get_lat_state_initial();
-            lon_state_sequence = maneuver.get_lon_state_sequence();
-            input_sequence = maneuver.get_lat_input_sequence();
-            data_sequence = {t_data_sequence input_sequence lon_state_sequence};
-            
-            % Estimate noise covariance matrix (R_hat) for Newton-Raphson
-            % step
-            y_pred = obj.sim_model(data_sequence, y_0, tspan, obj.OptData.params.CoeffsLat, obj.OptData.params.CoeffsLon);
-            residuals = obj.calc_residuals_lat(z, y_pred);
-            R_hat = obj.est_noise_covariance(residuals, N);
-            
-            % Minimize the cost by updating the parameters while keeping
-            % R_hat constant
-            
+        function obj = update_curr_maneuver_data(obj, maneuver)
+            obj.CurrManeuverData.t_data_sequence = maneuver.Time;
+            obj.CurrManeuverData.tspan = [maneuver.Time(1) maneuver.Time(end)];
+            obj.CurrManeuverData.N = length(obj.CurrManeuverData.t_data_sequence);
+            obj.CurrManeuverData.z = maneuver.get_lat_state_sequence();
+            obj.CurrManeuverData.y_0 = maneuver.get_lat_state_initial();
+            obj.CurrManeuverData.lon_state_sequence = maneuver.get_lon_state_sequence();
+            obj.CurrManeuverData.input_sequence = maneuver.get_lat_input_sequence();
+        end
+        
+        function [params_new, M] = do_newton_raphson_step(obj)            
             % Compute S (sensitivity matrices) for lateral parameters
-            disp("Computing sensitivies")
-            tic
-            S_all_i = zeros(N, obj.NOutputsLat, obj.NParamsLat);
+%             disp("Computing sensitivies")
+%             tic
+            S_all_i = zeros(obj.CurrManeuverData.N, obj.NOutputsLat, obj.NParamsLat);
             for j = 1:obj.NParamsLat
                 % Compute sensitivies by using first order central differences
-                delta_theta_j = obj.OptData.params.CoeffsLat(j) * obj.ParamPerturbation;
+                delta_theta_j = obj.CurrOptData.params.CoeffsLat(j) * obj.ParamPerturbation;
                 % Handle parameters set to 0
                 if delta_theta_j == 0
                    delta_theta_j = obj.ParamPerturbation;
                 end
-                perturbed_params_lat_pos = obj.create_perturbed_params_matrix(delta_theta_j, obj.OptData.params.CoeffsLat, j, 1);
-                perturbed_params_lat_neg = obj.create_perturbed_params_matrix(delta_theta_j, obj.OptData.params.CoeffsLat, j, -1);
+                perturbed_params_lat_pos = obj.create_perturbed_params_matrix(delta_theta_j, obj.CurrOptData.params.CoeffsLat, j, 1);
+                perturbed_params_lat_neg = obj.create_perturbed_params_matrix(delta_theta_j, obj.CurrOptData.params.CoeffsLat, j, -1);
                     
                 %tic
-                y_pos = obj.sim_model(data_sequence, y_0, tspan, perturbed_params_lat_pos, obj.OptData.params.CoeffsLon);
-                y_neg = obj.sim_model(data_sequence, y_0, tspan, perturbed_params_lat_neg, obj.OptData.params.CoeffsLon);
+                y_pos = obj.sim_model(perturbed_params_lat_pos, obj.CurrOptData.params.CoeffsLon);
+                y_neg = obj.sim_model(perturbed_params_lat_neg, obj.CurrOptData.params.CoeffsLon);
                 %toc
                 
                 dy_dtheta_j = (y_pos - y_neg) ./ (2 * delta_theta_j);
                 S_all_i(:,:,j) = dy_dtheta_j;
             end
             S_all_i = permute(S_all_i,[2 3 1]); % Get S in the correct dimensions: (n_outputs, n_params, n_timesteps)
-            toc
+            %toc
             
             % Calculate M (information matrix)
-            disp("Computing information matrix")
-            tic
-            M_all_i = zeros(obj.NParamsLat, obj.NParamsLat, N);
-            for timestep_i = 1:N
-                M_all_i(:,:,timestep_i) = S_all_i(:,:,timestep_i)' * (R_hat \ S_all_i(:,:,timestep_i));    
+            %disp("Computing information matrix")
+            %tic
+            M_all_i = zeros(obj.NParamsLat, obj.NParamsLat, obj.CurrManeuverData.N);
+            for timestep_i = 1:obj.CurrManeuverData.N
+                M_all_i(:,:,timestep_i) = S_all_i(:,:,timestep_i)' * (obj.CurrManeuverData.R_hat \ S_all_i(:,:,timestep_i));    
             end
             M = sum(M_all_i,3);
-            toc
+            %toc
             
             % Calculate g
-            disp("Computing g")
-            tic
-            g_all_i = zeros(obj.NParamsLat, 1, N);
-            for timestep_i = 1:N
-                g_all_i(:,:,timestep_i) = S_all_i(:,:,timestep_i)' * (R_hat \ residuals(timestep_i,:)');    
+            %disp("Computing g")
+            %tic
+            g_all_i = zeros(obj.NParamsLat, 1, obj.CurrManeuverData.N);
+            for timestep_i = 1:obj.CurrManeuverData.N
+                g_all_i(:,:,timestep_i) = S_all_i(:,:,timestep_i)' * (obj.CurrManeuverData.R_hat \ obj.CurrManeuverData.residuals(timestep_i,:)');    
             end
             g = sum(g_all_i,3);
-            toc
+            %toc
             
             % Compute param_update
             delta_theta = - M \ g;
             
             % Update parameters
-            params_new = obj.OptData.params.CoeffsLat + reshape(delta_theta, size(obj.OptData.params.CoeffsLat));
-            
-            % Estimate noise covariance matrix
-            y_pred = obj.sim_model(data_sequence, y_0, tspan, obj.OptData.params.CoeffsLat, obj.OptData.params.CoeffsLon);
-            residuals_new = obj.calc_residuals_lat(z, y_pred);
-            R_hat_new = obj.est_noise_covariance(residuals_new, N);
-            cost_new = obj.calc_cost_lat(R_hat_new, residuals_new);
+            params_new = obj.CurrOptData.params.CoeffsLat + reshape(delta_theta, size(obj.CurrOptData.params.CoeffsLat));
         end
         
         function perturbed_params_matrix = create_perturbed_params_matrix(~, delta_theta_j, params, j, sign)
@@ -123,11 +143,13 @@ classdef OutputErrorProblem
             perturbed_params_matrix(j) = perturbed_params_matrix(j) + sign * delta_theta_j;
         end
         
-        function y_pred = sim_model(obj, data_sequence, y_0, tspan, lat_params, lon_params)
+        function y_pred = sim_model(obj, lat_params, lon_params)
             obj.DynamicsModel = obj.DynamicsModel.set_params(lat_params, lon_params);
-            [t_data_seq, input_sequence, lon_state_seq] = data_sequence{:};
-            [t_pred, y_pred] = ode45(@(t,y) obj.DynamicsModel.dynamics(t, y, t_data_seq, input_sequence, lon_state_seq), tspan, y_0);
-            y_pred = interp1(t_pred, y_pred, t_data_seq); % change y_pred to correct time before returning
+            [t_pred, y_pred] = ode45(@(t,y) ...
+                obj.DynamicsModel.dynamics(t, y, ...
+                    obj.CurrManeuverData.t_data_sequence, obj.CurrManeuverData.input_sequence, obj.CurrManeuverData.lon_state_sequence), obj.CurrManeuverData.tspan, obj.CurrManeuverData.y_0...
+                    );
+            y_pred = interp1(t_pred, y_pred, obj.CurrManeuverData.t_data_sequence); % change y_pred to correct time before returning
         end
         
         function v = calc_residuals_lat(~, z, y_pred)
@@ -140,9 +162,9 @@ classdef OutputErrorProblem
                 
         function reached_convergence = check_for_convergence(obj, params_new, cost_new, R_hat_new)
             % TODO: Not finished. See Klein p 197 for details
-            abs_param_change = abs(obj.OptData.params - params_new);
-            abs_fval_change = abs((cost_new - obj.OptData.cost) / obj.OptData.cost);
-            abs_covar_change = abs(diag(obj.OptData.R_hat - R_hat_new)./(diag(obj.OptData.R_hat)));
+            abs_param_change = abs(obj.CurrOptData.params - params_new);
+            abs_fval_change = abs((cost_new - obj.CurrOptData.cost) / obj.CurrOptData.cost);
+            abs_covar_change = abs(diag(obj.CurrOptData.R_hat - R_hat_new)./(diag(obj.CurrOptData.R_hat)));
 
             if all(abs_param_change < 1e-5)
                 reached_convergence = true;
