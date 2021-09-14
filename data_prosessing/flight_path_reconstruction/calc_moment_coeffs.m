bc77c857dad5510d9493a279517af473f09df262
Bernhard Paus Græsdal <bernhardpg@gmail.com>
2021-09-14 14:20:01 +0200

    Fix sign mistake in lon coeff calculation

--- data_prosessing/flight_path_reconstruction/calc_moment_coeffs.m
+++ data_prosessing/flight_path_reconstruction/calc_moment_coeffs.m
@@ -5,7 +5,7 @@ function [c_l, c_m, c_n] = calc_moment_coeffs(p, q, r, u, v, w, p_dot, q_dot, r_
 
     %Q = calc_torque_pusher(n_p); Neglect moment from pusher motor
     c_l = (J_xx * p_dot - J_xz * (r_dot + p .* q) + q .* r * (J_zz - J_yy)) ./ (dyn_pressure * wingspan_m * planform_sqm);
-    c_m = (J_yy * q_dot - r .* p * (J_xx - J_zz) + J_xz * (p .^ 2 - r .^ 2)) ./ (dyn_pressure * mean_aerodynamic_chord_m * planform_sqm);
+    c_m = (J_yy * q_dot + r .* p * (J_xx - J_zz) + J_xz * (p .^ 2 - r .^ 2)) ./ (dyn_pressure * mean_aerodynamic_chord_m * planform_sqm);
     c_n = (J_zz * r_dot - J_xz * (p_dot - q .* r) + p .* q * (J_yy - J_xx)) ./ (dyn_pressure * wingspan_m * planform_sqm);
 end
 
