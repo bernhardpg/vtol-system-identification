7519d308557eb12f3dacf44e58f406a90a2bd889
Bernhard Paus Græsdal <bernhardpg@gmail.com>
2021-09-30 19:38:27 +0200

    Fix error metrics that were wrong for multiple maneuver types

--- model_validation/helper_functions/collect_mean_error_metrics_from_models.m
+++ model_validation/helper_functions/collect_mean_error_metrics_from_models.m
@@ -1,20 +1,27 @@
 function [mean_gof, mean_tic, mean_an, mean_mae, mean_rmse] = collect_mean_error_metrics_from_models(error_metrics, model_names, maneuver_types)
+    num_models = length(model_names);
+    
     mean_gof = {};
     mean_tic = {};
     mean_an = {};
     mean_mae = {};
     mean_rmse = {};
-    num_models = length(model_names);
-    for maneuver_type = maneuver_types
-        curr_error_metric = error_metrics.(maneuver_type);
-        for model_i = 1:num_models
-            model_name = model_names(model_i);
+    for model_i = 1:num_models
+        model_name = model_names(model_i);
+        mean_gof.(model_name) = [];
+        mean_tic.(model_name) = [];
+        mean_an.(model_name) = [];
+        mean_mae.(model_name) = [];
+        mean_rmse.(model_name) = [];
+        for maneuver_type = maneuver_types
+            curr_error_metric = error_metrics.(maneuver_type);
+
             [curr_mean_gof, curr_mean_tic, curr_mean_an, curr_mean_mae, curr_mean_rmse] = get_mean_error_metrics(curr_error_metric, model_name);
-            mean_gof.(model_name) = curr_mean_gof;
-            mean_tic.(model_name) = curr_mean_tic;
-            mean_an.(model_name) = curr_mean_an;
-            mean_mae.(model_name) = curr_mean_mae;
-            mean_rmse.(model_name) = curr_mean_rmse;
+            mean_gof.(model_name) = mean([mean_gof.(model_name); curr_mean_gof],1);
+            mean_tic.(model_name) = mean([mean_tic.(model_name); curr_mean_tic],1);
+            mean_an.(model_name) = mean([mean_an.(model_name); curr_mean_an],1);
+            mean_mae.(model_name) = mean([mean_mae.(model_name); curr_mean_mae],1);
+            mean_rmse.(model_name) = mean([mean_rmse.(model_name); curr_mean_rmse],1);
         end
     end
 end
